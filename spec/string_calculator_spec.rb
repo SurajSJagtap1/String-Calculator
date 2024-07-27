846faaf998b858444847baa1b631757ddcfde16c
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-07-27 18:16:17 +0530

    spec fixed: when only one number is passed in a string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+    it 'should return sum as same number if only one number is passed in a string' do
+        string_with_numbers = StringCalculator.new('4')
+        sum = string_with_numbers.add()
+        expect(sum).to eq (4)
+    end
+
     it 'should return sum of single digits separated by commas in a string' do
         string_with_numbers = StringCalculator.new('6,2,3,5')
         sum = string_with_numbers.add()
